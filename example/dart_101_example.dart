dcac415e56b5a99017e679f77d334e914c85617f
Ovidio R. Calvet <calveto@gmail.com>
2018-08-08 21:14:56 -0400

    Add parsing of line-number argument

--- example/dart_101_example.dart
+++ example/dart_101_example.dart
@@ -1,6 +1,19 @@
+import 'package:args/args.dart';
 import 'package:dart_101/dart_101.dart';
 
-main() {
+
+const lineNumber = 'line-number';
+ArgResults argResults;
+
+main(List<String> args) {
   var awesome = new Awesome();
   print('awesome: ${awesome.isAwesome}');
+  // Get command line arguments
+  final parser = new ArgParser()
+    ..addFlag(lineNumber, negatable: false, abbr: 'n');
+
+  argResults = parser.parse(args);
+
+  print('Argument ' + argResults[lineNumber].toString());
+
 }
